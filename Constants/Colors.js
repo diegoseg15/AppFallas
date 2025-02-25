babca4e5c30b4ac41d3c1a45ab0f7c1279fc1d99
diegoseg15 <diegofersv@gmail.com>
2025-02-25 16:01:22 +0100

    :art: Add color constants for light and dark mode

---
+++ Constants/Colors.js
@@ -0,0 +1,16 @@
+//Constantes de Colores de la aplicación tanto para modo claro como oscuro
+
+const COLORS = {
+  light: {
+    primary: '#F25041',
+    secondary: '#F2B441',
+    accent: '#5852F2'
+  },
+  dark: {
+    primary: '#B22222',
+    secondary: '#D2691E',
+    accent: '#483D8B'
+  }
+}
+
+export default COLORS
