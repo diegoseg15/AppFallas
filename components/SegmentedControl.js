6c557c8956ddfc525736203422ffcbcb130c3bba
diegoseg15 <diegofersv@gmail.com>
2025-03-01 19:48:41 +0100

    :toggle_button: Add SegmentedControl.js for Map/List toggle buttons

---
+++ components/SegmentedControl.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
+
+export default function SegmentedControl ({ vista, setVista }) {
+  return (
+    <View style={styles.segmentedControl}>
+      <TouchableOpacity
+        style={[styles.button, vista === 'mapa' && styles.activeButton]}
+        onPress={() => setVista('mapa')}
+      >
+        <Text
+          style={[styles.buttonText, vista === 'mapa' && styles.activeText]}
+        >
+          Mapa
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, vista === 'lista' && styles.activeButton]}
+        onPress={() => setVista('lista')}
+      >
+        <Text
+          style={[styles.buttonText, vista === 'lista' && styles.activeText]}
+        >
+          Lista
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  segmentedControl: {
+    flexDirection: 'row',
+    backgroundColor: '#F25041',
+    borderRadius: 25,
+    padding: 4,
+    width: 200,
+    alignSelf: 'center',
+    marginTop: 5
+  },
+  button: {
+    flex: 1,
+    paddingVertical: 5,
+    alignItems: 'center',
+    backgroundColor: '#F25041',
+    borderRadius: 20
+  },
+  activeButton: {
+    backgroundColor: 'white'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  activeText: {
+    color: '#F25041'
+  }
+})
