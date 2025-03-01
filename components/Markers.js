52beeea6580dfef917b05ca3c45b3bc5f1cb2d44
diegoseg15 <diegofersv@gmail.com>
2025-03-01 19:50:27 +0100

    :pushpin: Add Markers.js for rendering map markers and handling tooltip logic

---
+++ components/Markers.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Marker } from 'react-native-maps'
+
+export default function Markers ({ item, setShowTooltip, setDataItem }) {
+  // Estado para controlar la visibilidad del tooltip
+
+  if (!item) {
+    return null
+  }
+
+  // Función para manejar el clic en el Marker
+  const handleMarkerPress = () => {
+    setShowTooltip(true) // Mostrar el tooltip
+    setDataItem(item) // Guardar el item en el estado
+  }
+
+  return (
+    <>
+      <Marker
+        key={item.id_falla}
+        coordinate={{
+          latitude: item.geo_shape.geometry.coordinates[1],
+          longitude: item.geo_shape.geometry.coordinates[0]
+        }}
+        title={item.nombre}
+        icon={require('../assets/marker-icon.png')}
+        onPress={handleMarkerPress} // Manejar el clic en el Marker
+      />
+    </>
+  )
+}
