309e07d799e0aa3bb27444b12287e792f5b5765c
diegoseg15 <diegofersv@gmail.com>
2025-03-01 19:46:25 +0100

    :globe_with_meridians: Add MapViewComponent.js for MapView and Markers rendering

---
+++ components/MapViewComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import Markers from './Markers';
+
+export default function MapViewComponent({
+  mapViewRef,
+  initialRegion,
+  newRegion,
+  VLCitems,
+  setShowTooltip,
+  setDataItem,
+}) {
+  return (
+    <MapView
+      style={{ width: '100%', height: '100%' }}
+      provider={PROVIDER_GOOGLE}
+      ref={mapViewRef}
+      initialRegion={initialRegion}
+      showsUserLocation
+      showsMyLocationButton
+      region={newRegion}
+    >
+      {VLCitems.map((item) => (
+        <Markers
+          key={item.id_falla}
+          item={item}
+          setShowTooltip={setShowTooltip}
+          setDataItem={setDataItem}
+        />
+      ))}
+    </MapView>
+  );
+}
