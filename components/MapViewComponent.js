df7f00ec27de06ff5f3af9c0c5f994043635d517
diegoseg15 <diegofersv@gmail.com>
2025-03-11 16:54:10 +0100

    :sparkles: Add connection to children's faults API and display retrieved data

--- components/MapViewComponent.js
+++ components/MapViewComponent.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView from 'react-native-maps'
 import Markers from './Markers'
 
 export default function MapViewComponent ({
@@ -8,35 +7,41 @@ export default function MapViewComponent ({
   initialRegion,
   newRegion,
   VLCitems,
+  VLCitemsInfantil,
   setShowTooltip,
   setDataItem,
   categoriaSeleccionada
 }) {
-
-  
   //Filtramos por categoria
-  const itemsFiltrados = categoriaSeleccionada === "Todas"
-  ? VLCitems
-  : VLCitems.filter(item => item.seccion && item.seccion.includes(categoriaSeleccionada))
-  
-  
+  const itemsFiltrados =
+    categoriaSeleccionada === 'Todas'
+      ? VLCitems.concat(VLCitemsInfantil)
+      : VLCitems.concat(VLCitemsInfantil).filter(
+          item => item.seccion && item.seccion.includes(categoriaSeleccionada)
+        )
+
   return (
-    <MapView
-      style={{ width: '100%', height: '100%' }}
-      ref={mapViewRef}
-      initialRegion={initialRegion}
-      showsUserLocation
-      showsMyLocationButton
-      region={newRegion}
-    >
-      {itemsFiltrados.map(item => (
-        <Markers
-          key={item.id_falla}
-          item={item}
-          setShowTooltip={setShowTooltip}
-          setDataItem={setDataItem}
-        />
-      ))}
-    </MapView>
+    <>
+      <MapView
+        style={{ width: '100%', height: '100%' }}
+        ref={mapViewRef}
+        initialRegion={initialRegion}
+        showsUserLocation
+        showsMyLocationButton
+        // region={newRegion}
+      >
+        {itemsFiltrados.map(
+          item =>
+            item.objectid && (
+              <Markers
+                key={item.objectid}
+                item={item}
+                setShowTooltip={setShowTooltip}
+                setDataItem={setDataItem}
+              />
+            )
+        )}
+      </MapView>
+    </>
   )
 }
