6331c17d174983655611ac18eca7ab1e41a4b816
diegoseg15 <diegofersv@gmail.com>
2025-03-11 17:00:03 +0100

    :recycle: Update API connection to fetch children's faults data

--- Screens/Mapa.js
+++ Screens/Mapa.js
@@ -11,12 +11,12 @@ export default function Mapa ({ navigation }) {
   const [vista, setVista] = useState('mapa')
   const [mostrarCategorias, setMostrarCategorias] = useState(false)
   const mapView = useRef(null)
-  const [newRegion, setNewRegion] = useState(null)
   const [VLCitems, setVLCitems] = useState([])
+  const [VLCitemsInfantil, setVLCitemsInfantil] = useState([])
   const [showTooltip, setShowTooltip] = useState(false)
   const [dataItem, setDataItem] = useState(null)
   const [permission, setPermission] = useState(null)
-  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("Todas"); // Estado para la categoría
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('Todas') // Estado para la categoría
 
   const initialRegion = {
     latitude: 39.4699,
@@ -24,6 +24,8 @@ export default function Mapa ({ navigation }) {
     latitudeDelta: 2,
     longitudeDelta: 2
   }
+
+  const [newRegion, setNewRegion] = useState(initialRegion)
 
   useEffect(() => {
     const getCurrentLocation = async () => {
@@ -49,14 +51,34 @@ export default function Mapa ({ navigation }) {
     loadData()
   }, [])
 
-  const loadData = () => {
-    fetch(
-      'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/falles-fallas/records?limit=-1'
-    )
-      .then(response => response.json())
-      .then(responseJson => {
-        setVLCitems(responseJson.results)
-      })
+  const loadData = async () => {
+    let allRecordsMayor = []
+    let allRecordsInfantil = []
+    let limit = 100
+    let totalRecords = 349 // Sabemos que hay 349 registros
+
+    for (let offset = 0; offset < totalRecords; offset += limit) {
+      try {
+        const responseFallasMayor = await fetch(
+          `https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/falles-fallas/records?limit=${limit}&offset=${offset}`
+        )
+        const dataMayor = await responseFallasMayor.json()
+
+        const responseFallasInfantil = await fetch(
+          `https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/falles-infantils-fallas-infantiles/records?limit=${limit}&offset=${offset}`
+        )
+
+        const dataInfantil = await responseFallasInfantil.json()
+
+        allRecordsMayor = allRecordsMayor.concat(dataMayor.results)
+        allRecordsInfantil = allRecordsInfantil.concat(dataInfantil.results)
+      } catch (error) {
+        console.error('Error al obtener los datos:', error)
+      }
+    }
+
+    setVLCitems(allRecordsMayor) // Guarda los 349 registros en el estado
+    setVLCitemsInfantil(allRecordsInfantil)
   }
 
   const toggleCategorias = () => {
@@ -79,16 +101,17 @@ export default function Mapa ({ navigation }) {
             initialRegion={initialRegion}
             newRegion={newRegion}
             VLCitems={VLCitems}
+            VLCitemsInfantil={VLCitemsInfantil}
             setShowTooltip={setShowTooltip}
             setDataItem={setDataItem}
             categoriaSeleccionada={categoriaSeleccionada}
           />
         ) : (
-          <Lista 
-          navigation={navigation} 
-          VLCitems={VLCitems}
-          categoriaSeleccionada={categoriaSeleccionada}
-           />
+          <Lista
+            navigation={navigation}
+            VLCitems={VLCitems}
+            categoriaSeleccionada={categoriaSeleccionada}
+          />
         )}
       </View>
 
