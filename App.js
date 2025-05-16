4f55abb57e3d794908b25c4cc318db8ddc5baf79
diegoseg15 <diegofersv@gmail.com>
2025-05-16 02:31:58 +0200

    :sparkles: Add EditProfile screen and wrap app with UsuarioProvider context

--- App.js
+++ App.js
@@ -8,23 +8,28 @@ import Main from './Screens/Main';
 import Lista from './Screens/Lista';
 import DetalleFalla from './Screens/DetalleFalla';
 import { FallasProvider } from './context/FallasContext';
+import EditProfile from './Screens/EditProfile';
+import { UsuarioProvider } from './context/UsuarioContext';
 
 export default function App() {
   const Stack = createStackNavigator();
   return (
+    <UsuarioProvider>
 
-    <FallasProvider>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
-        <Stack.Screen name="Lista" component={Lista} options={{ headerShown: false }}/>
-        <Stack.Screen name="DetalleFalla" component={DetalleFalla} options={{ headerShown: false }}/>
-        
-      </Stack.Navigator>
-    </NavigationContainer>
-    </FallasProvider>
-    
+      <FallasProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+            <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
+            <Stack.Screen name="Lista" component={Lista} options={{ headerShown: false }} />
+            <Stack.Screen name="DetalleFalla" component={DetalleFalla} options={{ headerShown: false }} />
+            <Stack.Screen name='EditProfile' component={EditProfile} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FallasProvider>
+
+    </UsuarioProvider>
+
   );
 }
 
